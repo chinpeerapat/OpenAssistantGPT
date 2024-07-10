3305bfb2a443f306e51b20399e852416d64619b4
Akhilesh Rangani <akhileshrangani4@gmail.com>
2024-07-10 10:13:35 -0400

    CI/CD test push

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -15,7 +15,7 @@ export default function IndexPage() {
       <section data-aos="fade-up" className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 py-12 md:py-24 lg:py-32">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
           <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
-            Dashboard for your AI Tutor
+            Dashboard for AI Tutor
           </h1>
           <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
             Specifically made for Professor Alexa Alice Joubin
