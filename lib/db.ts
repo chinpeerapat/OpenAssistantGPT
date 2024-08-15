42893bd39f015c3b93726689a66788c036d59982
Akhilesh Rangani <akhileshrangani4@gmail.com>
2024-08-15 18:07:47 -0400

    add: prisma logging

--- lib/db.ts
+++ lib/db.ts
@@ -7,10 +7,14 @@ declare global {
 
 let prisma: PrismaClient
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient()
+  prisma = new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'], // Enable logging
+  })
 } else {
   if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient()
+    global.cachedPrisma = new PrismaClient({
+      log: ['query', 'info', 'warn', 'error'], // Enable logging in development too
+    })
   }
   prisma = global.cachedPrisma
 }
